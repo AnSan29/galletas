d5c507bf9bfc7d4c6bcdc445b1b56d8595a5db04
AnSan29 <andresdejesussantoyo@gmail.com>
2024-05-16 16:47:54 -0500

    funcionalidad a btn wsp

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
-import hamburgerMenu from "./menu-hamburguesa.js";
-import {eliminarItemCarrito, actualizarTotalCarrito, ocultarCarrito } from "./app.js";
-import ready from "./app.js";
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
+    const whatsappButton = document.getElementById('whatsappButton');
+    if (whatsappButton) {
+        console.log('Button found');
+        whatsappButton.addEventListener('click', function() {
+            console.log('Button clicked');
+            const phoneNumber = '573028658884'; // Reemplaza con tu número de WhatsApp en formato internacional
+            const message = 'Hola TortasMyJ estoy interesad@ en realizar un pedido.'; // Mensaje predefinido opcional
+            const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+            console.log('Redirecting to', url);
+            window.location.href = url;
+        });
+    } else {
+        console.error('Button not found');
+    }
 
-const d = document;
+    let botones = document.querySelectorAll('.boton-item');
 
-d.addEventListener("DOMContentLoaded", (e) => {
-    hamburgerMenu(".panel-btn", ".panel");
+    botones.forEach(function(boton) {
+        boton.addEventListener('click', function() {
+            alert('¡El botón ha sido clicado!');
+        });
+    });
 });
 
-ready();
-eliminarItemCarrito();
-actualizarTotalCarrito();
-ocultarCarrito();
 
 
